a639de7793c844b9d8aee51df4436366934b2ee3
mfr <mfr01606587@alibaba-inc.com>
2024-12-06 12:27:16 +0800

    docs: 安装 webpack-dev-server 和 html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -7,5 +8,19 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  devServer: {
+    static: './dist', // 静态文件目录
+    hot: true, // 启用热重载
+    open: true, // 自动打开浏览器
+    port: 3000 // 端口号
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+      title: 'Webpack App',
+      inject: 'body' // 资源注入位置
+    })
+  ],
 };
