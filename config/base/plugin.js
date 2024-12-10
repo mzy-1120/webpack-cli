0d76f83e8f7acab3256bc91b5dca177edd9613aa
meng <mfrwyy@163.com>
2024-12-10 22:28:06 +0800

    docs: 使用 copy-webpack-plugin 插件

--- config/base/plugin.js
+++ config/base/plugin.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const {DefinePlugin} = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin")
 
 
 module.exports = {
@@ -16,6 +17,27 @@ module.exports = {
       // filename: '**/index.html' // 自定义html名称(路径)
       // chunks: [], // 根据多入口，可手动导入要使用的chunk
       // publicPath:'' // 给script、style添加公共路径
+    }),
+
+
+    // XXX：将 public 下面的资源复制到 dist 目录去（除了index.html）
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "../../public"),
+          to: path.resolve(__dirname, "../../dist/public"),
+          toType: "dir",
+          noErrorOnMissing: true, // 不生成错误
+          globOptions: {
+            // 忽略文件
+            ignore: ["**/index.html"]
+          },
+          info: {
+            // 跳过terser压缩js
+            minimized: true
+          }
+        }
+      ]
     }),
 
     // XXX：Webpack 内置插件，用户创建全局常量
